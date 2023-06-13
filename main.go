8e4f8c3c0dff022153e8d8661818225aa68ea917
曲源成 <quyc@goodrain.com>
2023-06-13 13:46:58 +0800

    collect log

--- main.go
+++ main.go
@@ -3,36 +3,19 @@ package main
 import (
 	"bufio"
 	"context"
-	"encoding/json"
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"io"
-	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/watch"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 	"log"
 	"os"
-	"strings"
 	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
-
-type LogEntry struct {
-	Timestamp string  `json:"timestamp"`
-	Info      string  `json:"info"`
-	Request   Request `json:"request"`
-}
-
-type Request struct {
-	Path       string `json:"path"`
-	Comment    string `json:"comment"`
-	ClientIP   string `json:"clientIP"`
-	Method     string `json:"method"`
-	StatusCode int    `json:"statusCode"`
-	Latency    string `json:"latency"`
-	ReqID      string `json:"reqID"`
-}
 
 func main() {
 	var config *rest.Config
@@ -40,7 +23,6 @@ func main() {
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		logrus.Error("get InClusterConfig error:", err)
-		return
 	}
 	if config == nil {
 		config, err = clientcmd.BuildConfigFromFlags("", "/Users/qyc/go/src/awesomeProject/pod_log/kubeconfig")
@@ -52,69 +34,63 @@ func main() {
 
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		logrus.Error("Failed to create client: %v", err)
+		log.Fatalf("Failed to create client: %v", err)
+		return
 	}
 
-	namespace := os.Getenv("NAMESPACE")
-	watchPodCreation(clientset, namespace)
-}
-
-func watchPodCreation(clientset *kubernetes.Clientset, namespace string) {
-	watchInterface, err := clientset.CoreV1().Pods(namespace).Watch(context.Background(), v1.ListOptions{})
+	namespace := "helm"
+	podName := os.Getenv("POD_NAME")
+	if podName == "" {
+		podName = "image-demo-nginx-57d594dcd-vn9rm"
+	}
+	pod, err := clientset.CoreV1().Pods(namespace).Get(context.Background(), podName, v1.GetOptions{})
 	if err != nil {
-		logrus.Error("Failed to watch Pods: %v", err)
+		logrus.Errorf("get pod %v error:%v", podName, err)
+		return
 	}
-	for event := range watchInterface.ResultChan() {
-		pod, ok := event.Object.(*corev1.Pod)
-		if !ok {
-			logrus.Info("Failed to parse Pod event")
-			continue
+	containerName := pod.Spec.Containers[0].Name
+	req := clientset.CoreV1().Pods(namespace).GetLogs(podName, &corev1.PodLogOptions{
+		Container: containerName,
+		Follow:    true,
+	})
+
+	podLogs, err := req.Stream(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to open log stream: %v", err)
+	}
+	defer podLogs.Close()
+
+	stopCh := make(chan struct{})
+	go func() {
+		defer close(stopCh)
+		_, err = watchLogs(podLogs)
+		if err != nil {
+			log.Fatalf("Failed to watch logs: %v", err)
 		}
-		temp := strings.Split(pod.Name, "-")
-		tempList := temp[:len(temp)-2]
-		podNamePrefix := strings.Join(tempList, "-")
-		containerName := pod.Spec.Containers[0].Name
-		if strings.Contains(podNamePrefix, "nginx") && event.Type == watch.Added {
-			logrus.Infof("New Pod detected: %s", pod.Name)
-			go watchPodLogs(clientset, namespace, pod.Name, containerName)
-		}
+	}()
+
+	// Run for a specific duration, or until an error occurs
+	duration := 5 * time.Minute
+	select {
+	case <-time.After(duration):
+		log.Printf("Log monitoring duration reached. Exiting.")
+	case <-stopCh:
+		log.Printf("Log monitoring stopped due to an error.")
 	}
 }
 
-func watchPodLogs(clientset *kubernetes.Clientset, namespace, podName, containerName string) {
+func watchLogs(stream io.ReadCloser) (string, error) {
+	// Read log lines from the stream
+	reader := bufio.NewReader(stream)
 	for {
-		podLogs, err := clientset.CoreV1().Pods(namespace).GetLogs(podName, &corev1.PodLogOptions{
-			Container: containerName,
-			Follow:    true,
-		}).Stream(context.Background())
+		line, err := reader.ReadBytes('\n')
 		if err != nil {
-			logrus.Errorf("Failed to open log stream: %v", err)
-		}
-
-		reader := bufio.NewReader(podLogs)
-		for {
-			line, err := reader.ReadBytes('\n')
-			if err != nil {
-				if err == io.EOF {
-					break
-				}
-				logrus.Infof("Failed to read log line: %v", err)
+			if err == io.EOF {
 				break
 			}
-			log.Printf("Log line: %s", line)
-			var logEntry LogEntry
-			err = json.Unmarshal(line, &logEntry)
-			if err != nil {
-				logrus.Errorf("Failed to parse log entry: %s\n", line)
-				continue
-			}
-			if strings.HasPrefix(logEntry.Request.Path, "/charts/") && logEntry.Request.StatusCode == 200 {
-				//TODO: 调用openapi接口获取应用信息，统计安装数据
-				logrus.Info("========统计安装数据========")
-			}
+			return "", fmt.Errorf("failed to read log line: %v", err)
 		}
-
-		podLogs.Close()
-		time.Sleep(5 * time.Second)
+		fmt.Printf("Log line: %s", line)
 	}
+	return "", nil
 }
