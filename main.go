5ad071531c4adf69485bba771272b4f1c4d4c2ff
曲源成 <quyc@goodrain.com>
2023-06-13 21:40:11 +0800

    fix: get helm version openapi 3

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"fmt"
 	"github.com/sirupsen/logrus"
 	"io"
+	"io/ioutil"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -19,13 +21,6 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-type LogEntry struct {
-	Timestamp string `json:"timestamp"`
-	//Level     string `json:"level"`
-	//Message   string `json:"message"`
-	Request Request `json:"request"`
-}
-
 type Request struct {
 	Path       string `json:"path"`
 	Comment    string `json:"comment"`
@@ -34,6 +29,16 @@ type Request struct {
 	StatusCode int    `json:"statusCode"`
 	Latency    string `json:"latency"`
 	ReqID      string `json:"reqID"`
+}
+
+type Resp struct {
+	Code int           `json:"code"`
+	Msg  string        `json:"msg"`
+	Bean VersionDetail `json:"bean"`
+}
+
+type VersionDetail struct {
+	Version string `json:"version"`
 }
 
 func main() {
@@ -83,7 +88,7 @@ func main() {
 		}
 		podName = pod.Name
 		for _, c := range pod.Spec.Containers {
-			if strings.Contains(c.Name, "chartmuseum"){
+			if strings.Contains(c.Name, "chartmuseum") {
 				containerName = c.Name
 			}
 		}
@@ -110,7 +115,7 @@ func main() {
 	}()
 
 	// Run for a specific duration, or until an error occurs
-	duration := 5 * time.Minute
+	duration := 24 * time.Hour
 	select {
 	case <-time.After(duration):
 		log.Printf("Log monitoring duration reached. Exiting.")
@@ -141,12 +146,32 @@ func watchLogs(stream io.ReadCloser) (string, error) {
 		var request Request
 		err = json.Unmarshal([]byte(req), &request)
 		if err != nil {
-			logrus.Errorf("Failed to parse log entry: %s\n", line)
 			continue
 		}
 		if strings.HasPrefix(request.Path, "/charts/") && request.StatusCode == 200 {
-			//TODO: 调用openapi接口获取应用信息，统计安装数据
-			logrus.Info("========统计安装数据========")
+
+			// 解析出chart名字和版本 /charts/nginx-15.0.1.tgz
+			chart := strings.Split(request.Path, "/")[2]
+			ChartName := strings.Split(chart, "-")[0]
+			temp := strings.Split(chart, "-")[1]
+			ChartVersion := strings.TrimSuffix(strings.TrimSuffix(temp, ".tgz"), ".tgz.prov")
+			// 调用openapi接口获取应用信息，统计安装数据
+			url := os.Getenv("STORE_DOMAIN")
+			marketID := os.Getenv("MARKET_ID")
+			response, err := http.Get(fmt.Sprintf("%v/app-server/markets/%v/chart/%v/versions/%v/count?forInstall=true", url, marketID, ChartName, ChartVersion))
+			if err != nil {
+				fmt.Println("HTTP request error:", err)
+			}
+			defer response.Body.Close()
+			chartBody, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				fmt.Println("Failed to read response body:", err)
+			}
+			var resp Resp
+			_ = json.Unmarshal(chartBody, &resp)
+			if resp.Code == 200 && resp.Bean.Version == ChartVersion {
+				fmt.Println("Successfully counted installation data")
+			}
 		}
 	}
 	return "", nil
